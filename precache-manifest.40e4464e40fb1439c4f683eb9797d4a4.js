e626603261db8e3a9a2ce99d0168a5c4838b7fbb
Monika Swidzinska <monika@pxlwidgets-heroes.com>
2020-10-24 18:07:58 +0200

    Updates

---
+++ precache-manifest.40e4464e40fb1439c4f683eb9797d4a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ab684ffb3a9eaea20706e9f30e1ce9",
+    "url": "/poll/index.html"
+  },
+  {
+    "revision": "2d133b27665d433afacc",
+    "url": "/poll/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "60e00e52b12f03cca7ac",
+    "url": "/poll/static/css/main.1de2c59e.chunk.css"
+  },
+  {
+    "revision": "2d133b27665d433afacc",
+    "url": "/poll/static/js/2.d01ba868.chunk.js"
+  },
+  {
+    "revision": "5f2d6de39c39f8b6e46315c29574aa22",
+    "url": "/poll/static/js/2.d01ba868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e00e52b12f03cca7ac",
+    "url": "/poll/static/js/main.6a89ec71.chunk.js"
+  },
+  {
+    "revision": "a256e5fb122cb4d1d7ca",
+    "url": "/poll/static/js/runtime-main.8106ff49.js"
+  }
+]);

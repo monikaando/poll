327b8b9c23e0e1dd62f1fa264b3fa3d6d78fc170
Monika Swidzinska <monika@pxlwidgets-heroes.com>
2020-10-25 12:53:19 +0100

    styling for vote

--- poll-app/src/components/Vote.jsx
+++ poll-app/src/components/Vote.jsx
@@ -12,8 +12,8 @@ const Vote = () => {
           <div className="mb-4">
             <h5>Question?</h5>
           </div>
-          <div>
-            <div className="form-check mt-5 mb-3">
+          <div className="mt-4 pt-3">
+            <div className="answer-box form-check">
               <input
                 className="form-check-input"
                 type="radio"
@@ -25,30 +25,116 @@ const Vote = () => {
                 Default radio
               </label>
             </div>
-            <div className="form-check mb-2">
+            <div className="answer-box form-check">
               <input
                 className="form-check-input"
                 type="radio"
                 name="exampleRadios"
-                id="exampleRadios2"
+                id="exampleRadios1"
                 value="option1"
               />
               <label className="form-check-label" htmlFor="exampleRadios1">
                 Default radio
               </label>
             </div>
-            <div className="form-check mb-2">
+            <div className="answer-box form-check">
               <input
                 className="form-check-input"
                 type="radio"
                 name="exampleRadios"
-                id="exampleRadios3"
+                id="exampleRadios1"
                 value="option1"
               />
               <label className="form-check-label" htmlFor="exampleRadios1">
                 Default radio
               </label>
             </div>
+            <div className="answer-box form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="exampleRadios"
+                id="exampleRadios1"
+                value="option1"
+              />
+              <label className="form-check-label" htmlFor="exampleRadios1">
+                Default radio
+              </label>
+            </div>
+            <div className="answer-box form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="exampleRadios"
+                id="exampleRadios1"
+                value="option1"
+              />
+              <label className="form-check-label" htmlFor="exampleRadios1">
+                Default radio
+              </label>
+            </div>
+            <div className="answer-box form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="exampleRadios"
+                id="exampleRadios1"
+                value="option1"
+              />
+              <label className="form-check-label" htmlFor="exampleRadios1">
+                Default radio
+              </label>
+            </div>
+            <div className="answer-box form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="exampleRadios"
+                id="exampleRadios1"
+                value="option1"
+              />
+              <label className="form-check-label" htmlFor="exampleRadios1">
+                Default radio
+              </label>
+            </div>
+            <div className="answer-box form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="exampleRadios"
+                id="exampleRadios1"
+                value="option1"
+              />
+              <label className="form-check-label" htmlFor="exampleRadios1">
+                Default radio
+              </label>
+            </div>
+            <div className="answer-box form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="exampleRadios"
+                id="exampleRadios1"
+                value="option1"
+              />
+              <label className="form-check-label" htmlFor="exampleRadios1">
+                Default radio
+              </label>
+            </div>
+            <div className="answer-box form-check">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="exampleRadios"
+                id="exampleRadios1"
+                value="option1"
+              />
+              <label className="form-check-label" htmlFor="exampleRadios1">
+                Default radio
+              </label>
+            </div>
+            
+            
           </div>
         </div>
       </div>
